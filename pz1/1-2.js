3aaa4b2e76d63d6a15a70bb2f611b3e7967cd83a
Sofia Trocenko <trocenko3103@mail.ru>
2025-04-20 01:45:26 +0300

    first commit

---
+++ pz1/1-2.js
@@ -0,0 +1,28 @@
+
+function typeA(arg){
+    if (arg){
+        var type = typeof(arg);
+        switch (type){
+            case "string":
+                console.log("строка");
+                break;
+            case "object":
+                console.log("объект");
+                break;
+            case "number":
+                console.log("число");
+                break;
+            case "boolean":
+                console.log("логическое");
+                break;  
+            case "function":
+                console.log("функция");
+                break;  
+            case "null":
+            console.log("отсутствие значения");
+            break;
+        }
+    }
+    }
+    typeA(true);
+    
